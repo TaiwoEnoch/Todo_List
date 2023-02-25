08331407a6c02786f1ed42e8d8ff25eb1119b2cc
TaiwoEnoch <taiwoenoch0@gmail.com>
2023-02-25 09:09:43 +0100

    fix linter

--- webpack.config.js
+++ webpack.config.js
@@ -1,16 +1,18 @@
-const path = require("path");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-  entry: "./src/index.js",
-  plugins: [new HtmlWebpackPlugin({ 
-    template: "./src/index.html" 
-  })],
-  mode: "development",
+  entry: './src/index.js',
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+    }),
+  ],
+  mode: 'development',
   output: {
-    filename: "[name].bundle.js",
-    path: path.resolve(__dirname, "dist"),
+    filename: '[name].bundle.js',
+    path: path.resolve(__dirname, 'dist'),
   },
-  devServer: { static: "./dist" },
-  module: { rules: [{ test: /\.css$/i, use: ["style-loader", "css-loader"] }] },
+  devServer: { static: './dist' },
+  module: { rules: [{ test: /\.css$/i, use: ['style-loader', 'css-loader'] }] },
 };
